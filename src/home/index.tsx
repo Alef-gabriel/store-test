080e27b957654dfd9dc65af0a1a73939cfc68d4e
Alef gabriel <alefgabrielr@gmail.com>
2024-05-30 15:35:59 +0000

    add home page

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -5,7 +5,9 @@ function Home() {
   return (
     <div className="w-full m-0 p-0 flex flex-col justify-start">
       <Header />
-      <ProductGrid />
+      <div className="flex h-[170vh] justify-center p-[7.25rem] w-[90rem] bg-[#00000022]">
+        <ProductGrid />
+      </div>
     </div>
   );
 }
