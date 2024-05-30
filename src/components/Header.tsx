84d13bf6cc1e43ac526bec582badb8d5cc72c476
CodeSandbox <support@codesandbox.io>
2024-05-30 22:21:18 +0000

    Sandbox snapshot commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
 import cart from "../assets/cart.png";
-function Header() {
+
+interface ItemsProps {
+  setModalStatus: (status: boolean) => void;
+}
+
+const Header: React.FC<ItemsProps> = ({ setModalStatus }) => {
   return (
     <div className="w-[90rem] p-0 m-0 flex items-center h-[6.313rem] bg-[#0F52BA] gap-24">
       <div className="flex gap-1 w-2/4 pl-10 items-end">
@@ -7,13 +12,19 @@ function Header() {
         <p className="text-white text-xl font-light">Sistemas</p>
       </div>
       <div className="w-2/4 flex justify-end pr-10">
-        <div className="h-[2.813rem] w-[5.625rem] bg-white flex gap-2 rounded-lg justify-center items-center">
-          <img src={cart} alt="cart" />
-          <p className="font-bold text-lg">0</p>
-        </div>
+        <button
+          onClick={() => {
+            setModalStatus(true);
+          }}
+        >
+          <div className="h-[2.813rem] w-[5.625rem] bg-white flex gap-2 rounded-lg justify-center items-center">
+            <img src={cart} alt="cart" />
+            <p className="font-bold text-lg">0</p>
+          </div>
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import ProductGrid, { Product } from "../components/ProductGrid";
+import CartModal from "../components/CartModal";
 
 const Home: React.FC = () => {
   const [cartItems, addToCart] = useState<Product[]>([]);
+  const [modalStatus, setModalStatus] = useState<boolean>(false);
   return (
     <div className="w-full m-0 p-0 flex flex-col justify-start">
-      <Header />
+      <Header setModalStatus={setModalStatus} />
+      {modalStatus && (
+        <div className="w-full overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-end">
+          <CartModal products={cartItems} />
+        </div>
+      )}
       <div className="flex h-[170vh] justify-center p-[7.25rem] w-[90rem] bg-[#00000022]">
         <ProductGrid addToCart={addToCart} />
       </div>
